24563943dd6c896136f8decb5f80817ee134d39c
Ali <ali1998akbarzadeh@gmail.com>
2020-07-06 00:52:47 +0430

    initial commit

---
+++ build/precache-manifest.29c3546d51433b5a66f57c13e027477d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58d094b48edf38a1892d74f28830067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cc84215d88eee5a4e4f",
+    "url": "/static/css/main.13322230.chunk.css"
+  },
+  {
+    "revision": "711e499cd72a2cf7c4ae",
+    "url": "/static/js/2.57245ff1.chunk.js"
+  },
+  {
+    "revision": "0354a5099b1a6d3e50083b0a9287e2f8",
+    "url": "/static/js/2.57245ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc84215d88eee5a4e4f",
+    "url": "/static/js/main.f2a8e408.chunk.js"
+  },
+  {
+    "revision": "ccaefecb50f02e1fdd6c",
+    "url": "/static/js/runtime-main.6a369087.js"
+  }
+]);
